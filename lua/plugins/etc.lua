11d4284f231c5e8b18940ca30462f8e526a5a3d5
parkjbdev <dev@parkjb.com>
2023-10-16 23:18:34 +0900

    updates

---
+++ lua/plugins/etc.lua
@@ -0,0 +1,12 @@
+return {
+  { "Pocco81/auto-save.nvim", config = true },
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   opts = {
+  --     autoformat = false,
+  --   },
+  -- },
+  { "karb94/neoscroll.nvim", config = true },
+  { "wakatime/vim-wakatime" },
+  { "rcarriga/nvim-notify" },
+}

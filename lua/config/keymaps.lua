e45d8fbf8af24aaf806728e30ef10ce9a5644f82
parkjbdev <dev@parkjb.com>
2024-01-30 16:18:41 +0900

    feat: vox command & refactor: keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,16 +2,20 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-local function map(mode, lhs, rhs, opts)
-  local options = { noremap=true, silent=true }
-  if opts then
-    options = vim.tbl_extend('force', options, opts)
-  end
-  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
-end
+local Util = require("lazyvim.util")
+local map = Util.safe_keymap_set
 
 map('n', '<leader>ff', '<cmd>Telescope find_files<cr>')
 map('n', '<leader>fg', '<cmd>Telescope live_grep<cr>')
 map('n' ,'<leader>fb', '<cmd>Telescope buffers<cr>')
 map('n', '<leader>fh', '<cmd>Telescope help_tags<cr>')
 
+map("n", "<leader>vk", function()
+  local handle = io.popen("${HOME}/vms/vox -k")
+  handle:close()
+end, { desc = "vox kill" })
+
+map("n", "<leader>vs", function()
+  local handle = io.popen("${HOME}/vms/vox -s 0")
+  handle:close()
+end, { desc = "vox signal" })
